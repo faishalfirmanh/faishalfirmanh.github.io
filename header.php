bfea4ff16943372266a1b6502877e7e2a09ef77d
unknown <isalredarmy1@gmail.com>
2019-12-19 05:59:56 +0700

    ke2

---
+++ header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+    <script src="layout/scripts/jquery.min.js"></script>
+    <script src="layout/scripts/jquery.backtotop.js"></script>
+    <script src="layout/scripts/jquery.mobilemenu.js"></script>
+    <title></title>
+  </head>
+  <body>
+    <style type="text/css">
+    html {
+      scroll-behavior: smooth;
+    }
+    #sec1{
+      /* height: auto; */
+      background-image:url('images/demo/backgrounds/jus2.jpg');
+    }
+    #sec3{
+
+    }
+    .iniTex{
+      color: rgb(29,34,31);
+      font-weight: bold;
+    }
+    </style>
+    <div class="bgded overlay" style="background-image:url('images/demo/backgrounds/01.jpg');">
+      <!-- ################################################################################################ -->
+      <div class="wrapper row1">
+        <header id="header" class="hoc clear">
+          <!-- ################################################################################################ -->
+          <div id="logo" class="fl_left">
+            <h1><a href="index.html">Kopler :D</a></h1>
+          </div>
+          <nav id="mainav" class="fl_right">
+            <ul class="clear">
+              <li class="active"><a href="#sec1" style="">tentang</a></li>
+              <li><a class="drop" href="#">bola</a>
+                <ul>
+                  <li><a href="pages/gallery.html">Gallery</a></li>
+                  <li><a href="pages/gallery.html">Full Width</a></li>
+                  <li><a href="pages/gallery.html">Sidebar Left</a></li>
+                  <li><a href="pages/gallery.html">Sidebar Right</a></li>
+                  <li><a href="pages/gallery.html">Basic Grid</a></li>
+                </ul>
+              </li>
+              <li><a class="drop" href="#">programming</a>
+                <ul>
+                  <li><a href="pages/sidebar-left.html">Level 2</a></li>
+                  <li><a href="pages/sidebar-left.html">Level 2</a></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+          <!-- ################################################################################################ -->
+        </header>
+      </div>
+  </body>
+</html>
